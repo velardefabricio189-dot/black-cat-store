ea6a5014c19b11e400f6275c171ae817da67499d
Dylan <dylan.calle.dev@gmail.com>
2026-04-16 16:30:11 -0400

    feat: change background according to catalog, fallback background.jpg

--- src/app/(public)/categoria/[slug]/page.tsx
+++ src/app/(public)/categoria/[slug]/page.tsx
@@ -1,14 +1,16 @@
 import { getProductsByCategory } from "../../../lib/supabase/queries";
 import ProductGrid from "../../../components/catalog/ProductGrid";
+import { createClient } from "../../../lib/supabase/server";
 import Image from "next/image";
 export default async function CategoryPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
   const products = await getProductsByCategory(slug);
-
+  const supabase = await createClient();
+  const { data, error } = await supabase.from("categories").select("*").eq("slug", slug).single();
   return (
     <div>
       <Image
-        src="/background.jpg"
+        src={data?.public_url ?? `/background.jpg`}
         alt="Fondo"
         width={1500}
         height={1500}
